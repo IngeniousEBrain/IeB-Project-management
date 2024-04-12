646c081cd36325c5ab2d3429457c608d9e6ddcb0
Ekta <ekta@iebrain.com>
2024-04-12 17:28:05 +0530

    Password length fixed

--- frontend/src/Components/Privacy.jsx
+++ frontend/src/Components/Privacy.jsx
@@ -46,6 +46,8 @@ const Privacy = () => {
                 name="password"
                 type="password"
                 autoComplete="off"
+                minLength="8"
+                maxLength="13"
                 required
                 onChange={(e) => setPassword(e.target.value)}
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -68,6 +70,8 @@ const Privacy = () => {
                 name="password"
                 type="password"
                 autoComplete="off"
+                minLength="8"
+                maxLength="13"
                 required
                 onChange={(e) => setConfirmPassword(e.target.value)}
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
--- frontend/src/Screens/RegisterScreen.jsx
+++ frontend/src/Screens/RegisterScreen.jsx
@@ -36,10 +36,10 @@ const RegisterScreen = () => {
   const { userInfo } = useSelector((state) => state.auth);
   const { search } = useLocation();
   const sp = new URLSearchParams(search);
-  const redirect = sp.get("redirect") || "/";
+  const redirect = sp.get("redirect") || "/client";
 
   useEffect(() => {
-    if (userInfo) {
+    if (userInfo && userInfo.role === "client") {
       navigate(redirect);
     }
   }, [navigate, redirect, userInfo]);
@@ -73,12 +73,12 @@ const RegisterScreen = () => {
 
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 lg:px-4">
       <div className="sm:mx-auto sm:w-full sm:max-w-5xl">
         <form onSubmit={submitHandler} autoComplete="off">
           <div className="space-y-4">
             <h2 className="text-center text-2xl font-bold tracking-tight text-gray-900">
-              Sign up to create an account
+              Add Client
             </h2>
 
             <div className="border-b border-gray-900/10 pt-8 pb-4">
@@ -212,6 +212,8 @@ const RegisterScreen = () => {
                       name="password"
                       id="password"
                       autoComplete="new-password"
+                      minLength="8"
+                      maxLength="13"
                       onChange={(e) => setPassword(e.target.value)}
                       required
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -232,6 +234,8 @@ const RegisterScreen = () => {
                       name="confirm-password"
                       id="confrim-password"
                       autoComplete="off"
+                      minLength="8"
+                      maxLength="13"
                       onChange={(e) => setConfirmPassword(e.target.value)}
                       required
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -381,7 +385,7 @@ const RegisterScreen = () => {
             </div>
           </div>
 
-          <div className="mt-6 flex items-center justify-end">
+          <div className="my-6 flex items-center justify-end">
             <button
               type="submit"
               className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
